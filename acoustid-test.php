ed44fbc1a1926490594fa7ba6f9a45693d204fe0
mjkoncertdk-byte <mjkoncertdk@gmail.com>
2026-05-21 14:13:17 +0200

    Create acoustid-test.php

---
+++ acoustid-test.php
@@ -0,0 +1,30 @@
+<?php
+header('Content-Type: text/plain');
+
+$url = 'https://api.acoustid.org/v2/lookup';
+
+$data = http_build_query([
+    'client' => 'DIN_API_KEY_HER',
+    'duration' => 10,
+    'fingerprint' => 'test'
+]);
+
+$context = stream_context_create([
+    'http' => [
+        'method' => 'POST',
+        'header' => "Content-Type: application/x-www-form-urlencoded\r\n",
+        'content' => $data,
+        'timeout' => 30
+    ]
+]);
+
+$response = @file_get_contents($url, false, $context);
+
+echo "Response:\n";
+var_dump($response);
+
+echo "\nLast error:\n";
+var_dump(error_get_last());
+
+echo "\nHTTP response header:\n";
+var_dump($http_response_header ?? null);
